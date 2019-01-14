55ddbf79d2621eb8fd53d8090634cbee9c87bbba
mauriciocordeiro <mauriciocordeiro@live.com>
2019-01-14 16:14:54 -0300

    after 'services'

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,26 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
-	selector: 'app-heroes',
-	templateUrl: './heroes.component.html',
-	styleUrls: ['./heroes.component.scss']
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
 
-	// hero: Hero = { id: 1, name: 'Windstorm' };
-	
-	selectedHero: Hero;
-	heroes = HEROES;
+  selectedHero: Hero;
 
-	constructor() { }
+  heroes: Hero[];
 
-	ngOnInit() {
-	}
-	
-	onSelect(hero: Hero): void {
-		this.selectedHero = hero;
-	}
+  constructor(private heroService: HeroService) { }
 
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
 }
