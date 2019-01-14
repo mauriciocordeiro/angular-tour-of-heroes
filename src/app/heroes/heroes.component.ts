51d3e29fb75cc44adea1169f451846bb8e7e82b5
mauriciocordeiro <mauriciocordeiro@live.com>
2019-01-14 15:25:24 -0300

    before 'services'
    following Angular Tour of Heroes tutorial

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.scss']
+	selector: 'app-heroes',
+	templateUrl: './heroes.component.html',
+	styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+	// hero: Hero = { id: 1, name: 'Windstorm' };
+	
+	selectedHero: Hero;
+	heroes = HEROES;
 
-  ngOnInit() {
-  }
+	constructor() { }
+
+	ngOnInit() {
+	}
+	
+	onSelect(hero: Hero): void {
+		this.selectedHero = hero;
+	}
 
 }
